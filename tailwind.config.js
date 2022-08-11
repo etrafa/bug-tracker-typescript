db928c28b17a73ed030759bfbe2e025c25774031
Etem Senel <etemsenel96@hotmail.de>
2022-08-11 16:50:23 +0300

    first commit / settings were all done.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        mainGreen: "#29B498",
+        fbFillColor: "#1778F2",
+        strongRed: "#ff0000",
+      },
+    },
+  },
+  plugins: [],
+};
