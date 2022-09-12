617c0572af74186eef98cb99b2b7cf6532dc81ab
Etem Senel <etemsenel96@hotmail.de>
2022-09-12 07:27:38 +0300

    useGetDocsNested changed to generics.

--- src/customHooks/useGetDocsNested.ts
+++ src/customHooks/useGetDocsNested.ts
@@ -1,17 +1,13 @@
 //* READ FIRESTORE DATABASE NESTED
 
-import { collection, getDocs } from "firebase/firestore";
+import { collection, DocumentData, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db, useAuth } from "../firebase/firebaseConfig";
-import { IProject } from "../Interfaces/Firebase-Interfaces/ProjectInterface";
-import { IFirebaseUser } from "../Interfaces/Firebase-Interfaces/UserInterface";
 
-type UseGetDocsTypesArray = IFirebaseUser[] | IProject[] | null;
-
-export const useGetDocsNested = (colName: string, subCol: string) => {
-  const [dbData, setDbData] = useState<UseGetDocsTypesArray>(null);
+export function useGetDocsNested<T>(colName: string, subCol: string) {
+  const [dbData, setDbData] = useState<T[] | null>(null);
   const [loading, setLoading] = useState(false);
-  let FIREBASE_DATA: IFirebaseUser[] | IProject[] = [];
+  let FIREBASE_DATA: T[] = [];
   const currentUser = useAuth();
 
   useEffect(() => {
@@ -22,8 +18,7 @@ export const useGetDocsNested = (colName: string, subCol: string) => {
       res.docs.map(async (item) => {
         const docRef = collection(db, `${colName}/${item.id}/${subCol}`);
         const result = await getDocs(docRef);
-
-        result.docs.map((item) => {
+        result.docs.map((item: DocumentData) => {
           FIREBASE_DATA.push({ ...item.data(), id: item.id });
           setDbData([...FIREBASE_DATA]);
         });
@@ -34,4 +29,4 @@ export const useGetDocsNested = (colName: string, subCol: string) => {
     fetchData();
   }, [currentUser, colName, subCol]);
   return { dbData, loading };
-};
+}
