12cbc08c279d2a39bf35fa918e301cc7febf98c0
Etem Senel <etemsenel96@hotmail.de>
2022-09-14 00:35:19 +0300

    firebase add comment function added.

--- src/firebase/FirebaseTicketFunctions/firebaseAddComment.ts
+++ src/firebase/FirebaseTicketFunctions/firebaseAddComment.ts
@@ -1,1 +1,41 @@
-export const firebaseAddComment = () => {};
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  serverTimestamp,
+  where,
+} from "firebase/firestore";
+import { db } from "../firebaseConfig";
+
+export const firebaseAddTicketComment = async (
+  docID: string,
+  comment: string,
+  commentOwner: string,
+  belongedTicketID: string
+) => {
+  const colRef = collection(db, "projects");
+  const res = await getDocs(colRef);
+  res.docs.map(async (item) => {
+    const colRef = collection(db, "projects", item.ref.id, "tickets");
+    const q = query(colRef, where("id", "==", docID));
+
+    const result = await getDocs(q);
+
+    result.docs.map(async (item) => {
+      const childPath = item.ref;
+      const parentPath = childPath.parent;
+      const grandParentPath = parentPath.parent?.path;
+
+      if (grandParentPath) {
+        const collectionReference = collection(db, grandParentPath, "comments");
+        await addDoc(collectionReference, {
+          comment: comment,
+          commentOwner: commentOwner,
+          createdAt: serverTimestamp(),
+          belongedTicketID: belongedTicketID,
+        });
+      }
+    });
+  });
+};
