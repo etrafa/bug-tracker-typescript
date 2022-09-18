113320b465ecf6c06d46618587bf63bfb3285345
Etem Senel <etemsenel96@hotmail.de>
2022-09-18 08:01:28 +0300

    delete button style completed.

--- src/Utilities/Modals/ModalButton.tsx
+++ src/Utilities/Modals/ModalButton.tsx
@@ -22,9 +22,9 @@ const ModalButton = (props: ModalButtonProps) => {
       {props.isDeleteButtonActive && (
         <button
           type="submit"
-          className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          className="w-full bg-white text-strongRed font-bold rounded-lg text-sm px-5 py-2.5 mt-4 text-center hover:bg-red-600 hover:text-white hover:uppercase"
         >
-          {props.buttonText}
+          Delete this Ticket
         </button>
       )}
     </div>
